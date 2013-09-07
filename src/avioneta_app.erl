3e8f5111a90e1ce1cbf66a8260e6c285d1095ccd
Farruco Sanjurjo <madtrick@aire.local>
2013-09-07 12:05:19 +0200

    Initial commit

---
+++ src/avioneta_app.erl
@@ -0,0 +1,16 @@
+-module(avioneta_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    avioneta_sup:start_link().
+
+stop(_State) ->
+    ok.
