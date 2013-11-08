bb86f464eadabcf5cfdb5cfe2012c71d5a663e58
Farruco Sanjurjo <madtrick@gmail.com>
2013-11-08 17:11:45 +0100

    Create a module to handle avioneta config

--- src/avioneta_app.erl
+++ src/avioneta_app.erl
@@ -1,15 +1,16 @@
 -module(avioneta_app).
 -behaviour(application).
 
-%% Application callbacks
 -export([start/2, stop/1]).
 
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
+-define(APPLICATION, avioneta).
+-define(CONFIG_FILE, "avioneta.conf").
 
 start(_StartType, _StartArgs) ->
+  lager:info("Execution mode ~s", [fserlangutils_app:execution_mode(?APPLICATION)]),
+
   init_random_seed(),
+  init_config(),
   avioneta_sup:start_link().
 
 stop(_State) ->
@@ -17,3 +18,8 @@ stop(_State) ->
 
 init_random_seed() ->
   random:seed(erlang:now()).
+
+init_config() ->
+  {ok, Conf} = fserlangutils_app:read_in_priv(?APPLICATION, ?CONFIG_FILE),
+  avioneta_config:init(proplists:get_value(fserlangutils_app:execution_mode(?APPLICATION), Conf)).
+
---
+++ src/avioneta_config.erl
@@ -0,0 +1,19 @@
+-module(avioneta_config).
+
+-export([init/1]).
+-export([get/1, get/2]).
+
+-define(APPLICATION, avioneta).
+
+init(Config) ->
+  application:set_env(?APPLICATION, config, Config).
+
+get(Key) ->
+  {ok, Env}  = application:get_env(?APPLICATION, config),
+  kvc:path(Key, Env).
+
+get(Key, Default) ->
+  case avioneta_config:get(Key) of
+    undefined -> Default;
+    Value -> Value
+  end.
