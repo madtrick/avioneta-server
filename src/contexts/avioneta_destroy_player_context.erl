a1df93124b90e10bd10c068460a59202fb60c73c
Farruco Sanjurjo <madtrick@gmail.com>
2013-12-05 18:37:20 +0100

    Remove destroy player order

--- src/contexts/avioneta_destroy_player_context.erl
+++ src/contexts/avioneta_destroy_player_context.erl
@@ -6,11 +6,9 @@ call(CommandContextData, AvionetaGame, OriginChannel) ->
   UpdatedCommandContextData = avioneta_command_context_data:update(CommandContextData, [{origin, OriginChannel}, {avioneta_game, AvionetaGame}]),
   CommandData = avioneta_command_context_data:command_data(UpdatedCommandContextData),
 
-  PlayerId = (avioneta_command_data:runner(CommandData)):run(
+  _ = (avioneta_command_data:runner(CommandData)):run(
     avioneta_command_data:runner_data(CommandData), UpdatedCommandContextData
   ),
 
-  DestroyPlayerOrder = avioneta_destroy_player_order:new(PlayerId),
-
-  [{send_to_others, [DestroyPlayerOrder]}].
+  [].
 
